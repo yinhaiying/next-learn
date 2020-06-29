646ea44dd01d6bd5877b2b47d897e20a58cb9f5c
yinhaiying <18840842571@163.com>
2020-06-29 08:46:18 +0800

    next的学习

---
+++ lib/posts.tsx
@@ -0,0 +1,18 @@
+import fs,{ promises as fsPromise} from 'fs';
+import path from 'path';
+import  matter from 'gray-matter';
+
+// 读取文件内容
+export const getPosts = async () => {
+  const markdownDir:string = path.join(process.cwd(),'markdown');
+  const fileNames:string[] = await fsPromise.readdir(markdownDir);
+  const posts = fileNames.map((fileName) => {
+    const fullPath = path.join(markdownDir,fileName);
+    const id = fileName.replace(/\.md$/g,'');
+    const text = fs.readFileSync(fullPath,'utf-8');
+    const { data:{title,date}, content } = matter(text);
+    return {id,title,date};
+  });
+  console.log(posts)
+  return posts;
+}
---
+++ pages/api/v1/post.tsx
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getPosts } from "lib/posts";
+
+
+const Posts = async (req:NextApiRequest,res:NextApiResponse)=> {
+  const posts = await getPosts();
+  res.statusCode = 200;
+  res.setHeader('Content-Type','application/json');
+  res.write(JSON.stringify(posts));
+  res.end();
+}
+
+
+
+
+
+
+
+export default Posts;
